516ce04ba51d6e4691e208145102e5b79a2b3342
Aswin Sam M <aswinsam2810@gmail.com>
2023-08-04 18:57:49 +0530

    Addid the task to the local storage is done

--- script.js
+++ script.js
@@ -2,10 +2,39 @@ let task = document.querySelector("#task");
 let flag = document.querySelector("#flag");
 let inputDate = document.querySelector("#date");
 let priorityButton = document.querySelector("#priority");
-let 
+let addedTasks = document.querySelector(".added-tasks");
 
 let isPriority = false;
 const DATE = new Date();
+display();
+function display() {
+  if (localStorage.length > 0) {
+    addedTasks.style = "display:flex";
+    let totalItems = localStorage.length;
+    let items = "";
+    for (i = 0; i < totalItems; i++) {
+      let item = JSON.parse(localStorage.getItem(i));
+      if (item.priority) {
+        items += `<div class="all-tasks" style="border-bottom: 5px solid #800000;">
+        <h3 class="date-space">${item.date}</h3>
+        <h3>${item.task}</h3>
+        <button class="remove" id="${i}" onclick="remove(this.id)" title="Delete">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>`;
+      } else {
+        items += `<div class="all-tasks">
+        <h3 class="date-space">${item.date}</h3>
+        <h3>${item.task}</h3>
+        <button class="remove" id="${i}" onclick="remove(this.id)" title="Delete">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>`;
+      }
+    }
+    addedTasks.innerHTML = `<h1>TASKS</h1>` + items;
+  }
+}
 
 function priority() {
   if (!isPriority) {
@@ -17,6 +46,8 @@ function priority() {
     flag.src = "white.png";
     priorityButton.style = "background-color : transparent;color : black";
   }
+
+  return isPriority;
 }
 
 function isValid() {
@@ -30,7 +61,7 @@ function isValid() {
         return true;
       } else if (parseInt(date[0]) == year) {
         if (parseInt(date[1]) > month) {
-            return true;
+          return true;
         } else if (parseInt(date[1]) == month) {
           if (parseInt(date[2]) >= day) {
             return true;
@@ -56,7 +87,14 @@ function isValid() {
   }
 }
 function add() {
-    if(isValid()){
-
-    }
+  if (isValid()) {
+    let item = {
+      "task":task.value,
+      "date":inputDate.value,
+      "priority": priority()
+    };
+    let stringFormat = JSON.stringify(item);
+    localStorage.setItem(localStorage.length,stringFormat);
+    display();
+  }
 }
